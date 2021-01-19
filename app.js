41d31bde6ee91266dcc49f06441c954ea531df37
h-bar <663yan@gmail.com>
2021-01-19 00:29:23 -0500

    load and download from disk

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ const configs = {
 }
 const dir = 'templates/';
 
+function download(filename, text) {
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+
+  pom.style.display = 'none';
+  document.body.appendChild(pom);
+  pom.click();
+  document.body.removeChild(pom);
+}
+
 const onEdit = (content, template, config) => {
   preview = compile(content, template, config);
   document.getElementById("preview").srcdoc = preview;
@@ -33,7 +44,26 @@ const init = async (config) => {
     let content = event.target.value;
     onEdit(content, template, config)
   })
-  
+
+  const fileLoder = document.getElementById("fileLoader");
+  fileLoder.addEventListener('change', (event) => {
+    const reader = new FileReader();
+    reader.readAsText(event.target.files[0], "UTF-8");
+    reader.onloadend = ((event) => {
+      let content = event.target.result;
+      editor.value = content
+      onEdit(content, template, config)
+    })
+  });
+
+  document.getElementById("downloadBtn").addEventListener('click', (event) => {
+    const file = fileLoder.files[0];
+    let filename;
+    if (file !== undefined) filename = file.name
+
+    download(filename, editor.value)
+  })
+
   let content = await fetch(dir + config.name + '/default.rmk').then((resp) => resp.text())
   editor.value = content
   onEdit(content, template, config)
